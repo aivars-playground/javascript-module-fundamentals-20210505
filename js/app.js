02f4a3598527b8721a4718bb7c6793cdf63bef33
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2021-05-05 22:11:11 +0300

    adding parameter to iife

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-(() => {
+((name) => {
     //First attempt of optimization - reduce global scope pollution,
-    console.log("immediately invoked function evaluation => iife")
-})()
+    console.log(`immediately invoked function evaluation => iife, hello ${name}`)
+})('world')
+
